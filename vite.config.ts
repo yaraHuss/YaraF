d08e2e286561f51ed57fa1024a67e0d68b244836
Yara Hussein <yarahussien.dev@gmail.com>
2026-07-02 17:15:42 +0300

    Fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3001',
-      '/oauth': 'http://localhost:3001',
-    },
-  },
   build: {
     outDir: 'docs',
   },
